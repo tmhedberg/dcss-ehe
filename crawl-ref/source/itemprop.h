53c4de8b40dbc0f41d49a943884d967e06e8293e
Taylor Hedberg <t@tmh.cc>
2012-11-24 18:24:12 -0500

    Restore item_is_corpse function
    This is required by the corpse_at function, which was itself restored in
    5e3b515cd92d4172d4b3fe7f4f9db46e5070400f.

--- crawl-ref/source/itemprop.h
+++ crawl-ref/source/itemprop.h
@@ -139,6 +139,7 @@ bool item_is_rune(const item_def &item,
 bool item_is_unique_rune(const item_def &item) PURE;
 bool item_is_orb(const item_def &orb) PURE;
 bool item_is_horn_of_geryon(const item_def &item) PURE;
+bool item_is_corpse(const item_def &item) PURE;
 bool item_is_spellbook(const item_def &item) PURE;
 
 bool is_elemental_evoker(const item_def &item);
